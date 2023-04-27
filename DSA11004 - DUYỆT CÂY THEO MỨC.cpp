88d1f86c304f488b83587dc6d298ede5f0745ee1
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-04-27 10:14:18 +0700

    Create DSA11004 - DUYỆT CÂY THEO MỨC.cpp

---
+++ DSA11004 - DUYỆT CÂY THEO MỨC.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+#define run() \
+	int t;    \
+	cin >> t; \
+	while (t--)
+#define faster()                      \
+	cin.tie(0);                       \
+	ios_base::sync_with_stdio(false); \
+	cout.tie(0);
+
+struct Node
+{
+	int val;
+	Node *left, *right;
+};
+typedef Node *binTree;
+
+binTree createNode(int x)
+{
+	binTree tmp = new Node;
+	tmp->val = x;
+	tmp->left = tmp->right = NULL;
+	return tmp;
+}
+
+void insertNode(binTree root, int parent, int child, char c)
+{
+	if (root == NULL)
+		return;
+	if (root->val == parent)
+	{
+		if (c == 'L')
+			root->left = createNode(child);
+		else
+			root->right = createNode(child);
+	}
+	else
+	{
+		// nếu khác parent thì sang trái, phải tìm vị trí để insert
+		insertNode(root->left, parent, child, c);
+		insertNode(root->right, parent, child, c);
+	}
+}
+
+/**
+ * The function performs a level order traversal of a binary tree and prints the values of each node in
+ * the order they are visited.
+ */
+void levelOrder(binTree root)
+{
+	queue<binTree> Q;
+	Q.push(root);
+	while (!Q.empty())
+	{
+		binTree tmp = Q.front();
+		Q.pop();
+		cout << tmp->val << ' ';
+		if (tmp->left)
+			Q.push(tmp->left);
+		if (tmp->right)
+			Q.push(tmp->right);
+	}
+	cout << endl;
+}
+
+int n, a, b;
+main()
+{
+	faster();
+	run()
+	{
+		binTree root = NULL;
+		cin >> n;
+		int parent, child;
+		char c;
+
+		cin >> parent >> child >> c;
+		root = createNode(parent);
+		insertNode(root, parent, child, c);
+
+		n--;
+		while (n--)
+		{
+			cin >> parent >> child >> c;
+			insertNode(root, parent, child, c);
+		}
+
+		levelOrder(root);
+	}
+}
